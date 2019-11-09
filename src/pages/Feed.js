bbab67857f96c64e6299edb4a3d4ff9acfcf4ef2
Pedro Lopes <pedrolopes.info@gmail.com>
2019-11-09 18:45:39 -0300

    criar post, socket io

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,43 +1,78 @@
 import React, { Component } from 'react';
+import api from '../services/api';
+import io from 'socket.io-client';
 
 import more from '../assets/more.svg';
-import like from '../assets/more.svg';
+import like from '../assets/like.svg';
 import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 import './Feed.css';
 
 class Feed extends Component {
+    state = {
+        feed: []
+    }
+
+    async componentDidMount() {
+        this.registerToSocket();
+
+        const response = await api.get('posts');
+        this.setState({ feed: response.data });
+    }
+
+    registerToSocket = () => {
+        const socket = io('http://localhost:3333');
+        
+        socket.on('post', newPost => {
+            this.setState({ feed: [newPost, ...this.state.feed] });
+        });
+
+        socket.on('like', likedPost => {
+            this.setState({ 
+                feed: this.state.feed.map(post => post._id === likedPost._id ? likedPost : post)
+            });
+        });
+    }
+    
+    handleLike = id => {
+        api.post(`/posts/${id}/like`);
+    }
+
     render() {
         return (
             <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Diego Fernandes</span>
-                            <span className="place">Rio do Sul</span>
-                        </div>
+                { this.state.feed.map(post => (
+                    <article key={post._id}>
+                        <header>
+                            <div className="user-info">
+                                <span>{post.author}</span>
+                                <span className="place">{post.place}</span>
+                            </div>
 
-                        <img src={more} alt="Mais" />
-                    </header>
+                            <img src={more} alt="Mais" />
+                        </header>
 
-                    <img src="http://localhost:3333/files/pinkfloyd.jpg" alt="Pinkfloyd" />
+                        <img src={`http://localhost:3333/files/${post.image}`} alt="Pinkfloyd" />
 
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="Like" />
-                            <img src={comment} alt="Comentar" />
-                            <img src={send} alt="Enviar" />
-                        </div>
+                        <footer>
+                            <div className="actions">
+                                <button type="button" onClick={() => this.handleLike(post._id)}>
+                                    <img src={like} alt="Like" />
+                                </button>
+                                <img src={comment} alt="Comentar" />
+                                <img src={send} alt="Enviar" />
+                            </div>
 
-                        <strong>900 curtidas</strong>
+                            <strong>{post.likes} curtidas</strong>
 
-                        <p>
-                            Um post muito massa da Omnistack!
-                            <span>#react #omnistack #top</span>
-                        </p>
-                    </footer>
-                </article>
+                            <p>
+                                {post.description}
+                                <span>{post.hashtags}</span>
+                            </p>
+                        </footer>
+                    </article>
+                )) }
             </section>
         );
     }
